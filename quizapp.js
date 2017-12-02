de1327d83e176bbbf13a9dcb2c894a8ac6a0a4e7
karinacsanchez <32423123+karinacsanchez@users.noreply.github.com>
2017-12-02 11:06:12 -0800

    Add files via upload

---
+++ quizapp.js
@@ -0,0 +1,18 @@
+
+displays start button 
+
+clicks button
+
+shows first question in the array, displays page number, displays number of right guesses
+
+if - click wrong anser, displays right answer and control buttons.
+
+else - click right answer, displays control buttons.
+
+clicks 'next'
+
+displays next question in the array
+
+last page shows the final number of right guesses
+
+
